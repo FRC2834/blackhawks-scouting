813ddc8dda16b18072165b87679e7c0a7aaacd6b
Aidan Sun <aidansun05@gmail.com>
2023-02-25 13:48:35 -0500

    Add team heading to each page

--- src/components/FormPage.vue
+++ src/components/FormPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div v-show="show">
     <h1 class="page-heading">{{ config.data.heading ?? "Scouting" }}</h1>
+    <h3 v-if="teamDesc?.length > 0" class="page-heading">Team: {{ teamDesc }}</h3>
     <img v-if="config.data.logo" :src="absoluteLogoPath" alt="Cannot load logo file" class="center" />
     <h2 class="page-heading">{{ title }}</h2>
     <div class="grid">
@@ -18,6 +19,8 @@ const props = defineProps<{
 
 const config = useConfigStore();
 const widgets = useWidgetsStore();
+
+const teamDesc = $computed(() => widgets.values.find(i => i.name == "Team")?.value.replaceAll(",", ", "));
 
 // Get the full path to the logo image
 const absoluteLogoPath = $computed(() => `${import.meta.env.BASE_URL}assets/${config.data.logo}`);
